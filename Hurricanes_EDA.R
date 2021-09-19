608c15dc0350907049fe94289169e6fe2ede8389
Vipul Gharde <vipul.gharde@gmail.com>
2021-09-18 22:22:44 -0400

    Added Plot for Category 4 Hurricanes

--- Hurricanes_EDA.R
+++ Hurricanes_EDA.R
@@ -1,4 +1,18 @@
 # Plot Bar plots of Hurricanes by Decade
+Cat4.Atlantic.decade <-
+  read.csv('datasets/category4_atlantic_by_decade.csv')
+Cat4.Atlantic.decade
+
+barplot(
+  Cat4.Atlantic.decade$frequency,
+  names.arg = Cat4.Atlantic.decade$decade,
+  main = 'Category 4 Hurricanes by Decade',
+  xlab = 'Decade',
+  ylab = 'Frequency',
+  las = 2
+)
+grid()
+
 Cat5.Atlantic.decade <-
   read.csv('datasets/category5_atlantic_by_decade.csv')
 Cat5.Atlantic.decade
